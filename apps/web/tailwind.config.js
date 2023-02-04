69e458730aba244acbeb34524d03d98a31244251
Chia1104 <yuyuchia7423@gmail.com>
2023-02-04 20:24:21 +0800

    :test_tube: Testing.

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,16 +1,14 @@
-const config = require("@chia-stack/tailwind-config");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [`src/**/*.{js,ts,jsx,tsx}`],
-  ...config,
   theme: {
     extend: {
       colors: {
         primary: "#00e1ff",
         secondary: "#ff00e1",
-        ...config.theme.extend.colors,
+        ctw_primary: "#00e1ff",
       },
     },
   },
+  plugins: [require("@chia-stack/tailwind-config")],
 };
