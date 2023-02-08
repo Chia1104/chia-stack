92bc8bf0de9f8e1ec6117e3fe7fbe5c5ff78aa23
Chia1104 <yuyuchia7423@gmail.com>
2023-02-08 20:47:09 +0800

    :adhesive_bandage: Update `tailwind.config.js`.

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,13 +1,17 @@
+const config = require("@chia-stack/tailwind-config");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [`src/**/*.{js,ts,jsx,tsx}`],
+  ...config,
   theme: {
     extend: {
       colors: {
         primary: "#00e1ff",
         secondary: "#ff00e1",
+        ...config.theme.extend.colors,
       },
     },
   },
-  plugins: [require("@chia-stack/tailwind-config")],
+  // plugins: [config],
 };
