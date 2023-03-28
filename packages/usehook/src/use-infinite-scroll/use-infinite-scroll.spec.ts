5d3a3658bb6eed9497abb53dd5581af951f53a7a
Chia1104 <yuyuchia7423@gmail.com>
2023-03-28 15:29:27 +0800

    :construction: wip

--- packages/usehook/src/use-infinite-scroll/use-infinite-scroll.spec.ts
+++ packages/usehook/src/use-infinite-scroll/use-infinite-scroll.spec.ts
@@ -17,6 +17,15 @@ describe("useInfiniteScroll", () => {
   let onLoadMoreMock: Mock;
   let options: UseInfiniteScrollOptions;
 
+  const IntersectionObserverMock = vi.fn(() => ({
+    disconnect: vi.fn(),
+    observe: vi.fn(),
+    takeRecords: vi.fn(),
+    unobserve: vi.fn(),
+  }));
+
+  vi.stubGlobal("IntersectionObserver", IntersectionObserverMock);
+
   beforeEach(() => {
     onLoadMoreMock = vi.fn();
     options = {
