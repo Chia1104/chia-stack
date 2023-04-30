8bdc5bc4d463cb7558bb3ee056904549be65698c
Chia1104 <yuyuchia7423@gmail.com>
2023-05-01 00:36:19 +0800

    wip(ui): change to tailwind-variants

--- packages/react-ui/src/button/button.tsx
+++ packages/react-ui/src/button/button.tsx
@@ -3,9 +3,77 @@ import { cn } from "../utils";
 import { type VariantProps, cva } from "class-variance-authority";
 import { motion, type MotionProps } from "framer-motion";
 
+// @ts-ignore wip
+import { tv, type VariantProps as TVariantProps } from "tailwind-variants";
+
 interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {}
+
+const baseVariants = tv({
+  base: "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed",
+});
+
+// @ts-ignore wip
+const newVariants = tv({
+  extend: baseVariants,
+  variants: {
+    color: {
+      primary: "bg-primary text-white hover:bg-primary/80",
+      secondary: "bg-secondary text-white hover:bg-secondary/80",
+      success: "bg-success text-white hover:bg-success/80",
+      danger: "bg-danger text-white hover:bg-danger/80",
+      warning: "bg-warning text-white hover:bg-warning/80",
+      info: "bg-info text-white hover:bg-info/80",
+    },
+    outline: {
+      true: "bg-transparent border border-secondary dark:border-primary hover:bg-secondary/10 dark:hover:bg-primary/10",
+    },
+    ghost: {
+      true: "bg-transparent hover:bg-secondary/10 dark:hover:bg-primary/10 dark:text-slate-100 dark:hover:text-slate-100 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent",
+    },
+    link: {
+      true: "bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent",
+    },
+  },
+  compoundVariants: [
+    {
+      color: "success",
+      outline: true,
+      class: "border-success hover:bg-success/10",
+    },
+    {
+      color: "success",
+      ghost: true,
+      class: "hover:bg-success/10 dark:hover:bg-success/10",
+    },
+    {
+      color: "danger",
+      outline: true,
+      class: "border-danger hover:bg-danger/10",
+    },
+    {
+      color: "danger",
+      ghost: true,
+      class: "hover:bg-danger/10 dark:hover:bg-danger/10",
+    },
+    {
+      color: "warning",
+      outline: true,
+      class: "border-warning hover:bg-warning/10",
+    },
+    {
+      color: "warning",
+      ghost: true,
+      class: "hover:bg-warning/10 dark:hover:bg-warning/10",
+    },
+    {
+      color: "info",
+      outline: true,
+      class: "border-info hover:bg-info/10",
+    },
+  ],
+});
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed",
