c5dfee3f1c9992bc0d3151d3360594c0464ebb83
Chia1104 <yuyuchia7423@gmail.com>
2023-06-26 01:01:11 +0800

    feat(templates): optimize dockerfile

--- templates/nest-gql-codefirst/src/main.ts
+++ templates/nest-gql-codefirst/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import "@total-typescript/ts-reset";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
