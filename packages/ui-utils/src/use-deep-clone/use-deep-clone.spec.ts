c6e176ae61e2bb0ad8cac91127903fc54c2d3aa0
Chia1104 <yuyuchia7423@gmail.com>
2023-07-22 21:47:44 +0800

    fix: fix 'useDeepClone' test

--- packages/ui-utils/src/use-deep-clone/use-deep-clone.spec.ts
+++ packages/ui-utils/src/use-deep-clone/use-deep-clone.spec.ts
@@ -5,6 +5,6 @@ import useDeepClone from "./use-deep-clone";
 describe("useDeepClone", () => {
   it("should return a deep cloned value", () => {
     const { result } = renderHook(() => useDeepClone({ foo: "bar" }));
-    expect(result.current[0]).toBe({ foo: "bar" });
+    expect(result.current[0]).toStrictEqual({ foo: "bar" });
   });
 });
